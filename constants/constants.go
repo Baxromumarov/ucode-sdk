3adcf21ef276fcfcaea0a6ef7322cc9be6ce641a
baxromumarov <baxromumarov10@gmail.com>
2024-05-31 09:55:02 +0500

    removed token

--- constants/constants.go
+++ constants/constants.go
@@ -8,7 +8,7 @@ const (
 	UrlField        = "https://api.admin.u-code.io/v2/fields/"
 	UrlRelation     = "https://api.admin.u-code.io/v2/relations/"
 	GetFields       = "https://api.admin.u-code.io/v1/field"
-	Token           = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfcGxhdGZvcm1faWQiOiIiLCJjbGllbnRfdHlwZV9pZCI6ImI3NDVlNjY0LThmMTEtNGRhYS1iNWQ5LTQ1NWNiZGFjODRjNSIsImRhdGEiOiJhZGRpdGlvbmFsIGpzb24gZGF0YSIsImV4cCI6MTcxNzIxNDQyOSwiaWF0IjoxNzE3MTI4MDI5LCJpZCI6IjdhZGUwNTQwLWZmZWQtNDY0Yy05M2Q4LTA4NDIzMDY2ODdkZiIsImlwIjoiYWRkaXRpb25hbCBqc29uIGRhdGEiLCJsb2dpbl90YWJsZV9zbHVnIjoiXCJ1c2VyXCIiLCJwcm9qZWN0X2lkIjoiNjUyOGIwNmMtMjg0Mi00OTA1LWEwYWEtYmM0MDY1M2FmYzU4Iiwicm9sZV9pZCI6ImI0YmI4YTlhLTNjNDctNGNjNC1hNmJhLTZiMGU4YjA5MTIyMCIsInRhYmxlcyI6W10sInVzZXJfaWQiOiI0MWFlYzc3OS02ZWIwLTQ4YjMtYmMzMy05Mzk0ZTFiZjYwNDcifQ.RwV7wog9aR8VsLIxHFVbxs9-IUfTX311BQlx7S1pL2c"
+	Token           = ""
 )
 
 var (
